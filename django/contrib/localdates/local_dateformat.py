a733d241b122fd5396d9acb6197f515dd49bbbe4
orestis <orestis@2d4215e5-4f35-0410-b94f-7947a7918f2f>
2007-07-17 10:13:32 +0000

    translatable format strings
    git-svn-id: https://django-localdates.googlecode.com/svn/trunk@3 2d4215e5-4f35-0410-b94f-7947a7918f2f

--- django/contrib/localdates/local_dateformat.py
+++ django/contrib/localdates/local_dateformat.py
@@ -1,6 +1,7 @@
 
 
 from django.utils.dateformat import TimeFormat, DateFormat
+from django.utils.translation import ugettext as _
 import re
 
 
@@ -8,7 +9,48 @@ local_re = re.compile(r'(\{\w{2}\})')
 
 
 
-standard_format_strings = {
+local_formats = (
+    'FULL_DATE', 
+    'ABBR_DATE', 
+    'NUM_DATE', 
+    
+    'FULL_DATETIME', 
+    'ABBR_DATETIME',
+    'NUM_DATETIME',
+    
+    'FULL_TIME',
+    
+    'FULL_YEARMONTH',
+    'ABBR_YEARMONTH',
+    'NUM_YEARMONTH',
+    
+    'FULL_MONTHDAY',
+    'ABBR_MONTHDAY',
+    'NUM_MONTHDAY',
+    )
+
+#to trigger translation    
+dummy_formats = (
+    _('FULL_DATE'), 
+    _('ABBR_DATE'), 
+    _('NUM_DATE'), 
+   
+    _('FULL_DATETIME'), 
+    _('ABBR_DATETIME'),
+    _('NUM_DATETIME'),
+    
+    _('FULL_TIME'),
+    
+    _('FULL_YEARMONTH'),
+    _('ABBR_YEARMONTH'),
+    _('NUM_YEARMONTH'),
+    
+    _('FULL_MONTHDAY'),
+    _('ABBR_MONTHDAY'),
+    _('NUM_MONTHDAY'),
+    )
+
+default_format_strings = {
     'FULL_DATE':'l, j {Fp}, Y',
     'ABBR_DATE':'j M, Y',
     'NUM_DATE':'j/n/Y',
@@ -18,17 +60,35 @@ standard_format_strings = {
     'NUM_DATETIME':'j/n/Y, P',
     
     'FULL_TIME':'P',
+    
     'FULL_YEARMONTH':'F Y',
     'ABBR_YEARMONTH':'M Y',
     'NUM_YEARMONTH':'n/Y',
+    
     'FULL_MONTHDAY':'j {Fp}',
     'ABBR_MONTHDAY':'j M',
     'NUM_MONTHDAY':'j/n',
 
 }
 
+
+# this is after the django.utils.translation.trans_real.get_date_formats
+# TODO take into consideration the settings date formats
+def get_local_formats():
+    from django.conf import settings
+    formats = {}
+    
+    for format in defaul_format_strings:
+        result = ugettext(format)
+        if result == format:
+            result = default_format_string[format]
+        formats[format]=result
+    
+    return formats
+
+
 #this construcs the regexp string like this: r"(\{FULL_DATE\}|\{ABBR_DATE\}|...)"
-local_standard_string = r'('+(r'|'.join([r'\{%s\}'%key for key in standard_format_strings]))+r')'
+local_standard_string = r'('+(r'|'.join([r'\{%s\}'%key for key in get_local_formats()]))+r')'
 
 local_standard_re = re.compile(local_standard_string)
 
@@ -39,7 +99,7 @@ class LocalFormatter(object):
         # replace the standard strings with their expanded versions
         standard_pieces = local_standard_re.findall(formatstr)
         for piece in standard_pieces:
-            formatstr = formatstr.replace(piece, standard_format_strings[piece[1:-1]])
+            formatstr = formatstr.replace(piece, get_local_formats[piece[1:-1]])
         
         # put the local values in the format str
         local_pieces = local_re.findall(formatstr)
@@ -61,10 +121,11 @@ class LocalDateFormat(LocalFormatter, DateFormat):
         self._format = DateFormat.format
         super(DateFormat, self).__init__(*args, **kwargs)
     
+class LocalTimeFormat(LocalFormatter, TimeFormat):
+    def __init__(self, *args, **kwargs):
+        self._format = TimeFormat.format
+        super(TimeFormat, self).__init__(*args, **kwargs)
     
-#    def Fp(self):
-#        return MONTHS_POS[self.data.month]
-        
     
     
 def format(value, format_string, locale=None):
